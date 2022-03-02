03ca74b9937f36ae64b08b6b58f4edfd65af4301
zouhair <ettarak.zouhair@gmail.com>
2022-03-02 21:38:04 +0100

    Display Form course and closed it

--- src/components/course/Course.vue
+++ src/components/course/Course.vue
@@ -1,9 +1,14 @@
 <template lang="">
-    <div class="row">
-        <div class="col-md-6 mx-auto">
+    <div class="row"  >
+        <div class="col-md-6 mx-auto" v-if="showForm">
             <AddCours />
         </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-md-12 text-right">
+            <button class="btn btn-success btn-sm" type="button" @click="displayForm">New</button>
         </div>
+    </div>
 
     <div class="row my-2">
       <ListCourses />
@@ -16,7 +21,17 @@ export default {
     components: {
         AddCours,
         ListCourses
-    }
+    },
+    data() {
+        return {
+            showForm: false,
+        }
+    },
+    methods: {
+        displayForm() {
+            this.showForm = !this.showForm;
+        }
+    },
 }
 </script>
 <style lang="">
